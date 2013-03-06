6fd0f3263015731010c17becbb1f3450de3a0f81
Danny Baumann <dannybaumann@web.de>
2013-03-06 16:14:59 +0100

    Make going to call log after call optional.
    JIRA: CYAN-528
    Change-Id: I1cab653e9ac28b767ae3aeebceeaf822d9438b4c

--- src/com/android/phone/InCallScreen.java
+++ src/com/android/phone/InCallScreen.java
@@ -2813,6 +2813,7 @@ public class InCallScreen extends Activity
 
                 if ((mLastDisconnectCause != Connection.DisconnectCause.INCOMING_MISSED)
                         && (mLastDisconnectCause != Connection.DisconnectCause.INCOMING_REJECTED)
+                        && PhoneUtils.PhoneSettings.showCallLogAfterCall(this)
                         && !isPhoneStateRestricted()
                         && PhoneGlobals.sVoiceCapable) {
                     final Intent intent = mApp.createPhoneEndIntentUsingCallOrigin();
